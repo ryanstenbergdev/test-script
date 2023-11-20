8f48220f51cd60d6ac867329c4b506ae276bb16a
ryanstenbergdev <121960717+ryanstenbergdev@users.noreply.github.com>
2023-11-20 07:07:31 -0600

    Create test.js

---
+++ test.js
@@ -0,0 +1,17 @@
+// File: trust-form.js
+
+document.write(`
+  <!-- TrustedForm -->
+  <script type="text/javascript">
+  (function() {
+  var tf = document.createElement('script');
+  tf.type = 'text/javascript'; tf.async = true;
+  tf.src = ("https:" == document.location.protocol ? 'https' : 'http') + "://api.trustedform.com/trustedform.js?field=xxTrustedFormCertUrl&ping_field=xxTrustedFormPingUrl&l=" + new Date().getTime() + Math.random();
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(tf, s);
+  })();
+  </script>
+  <noscript>
+  <img src="https://api.trustedform.com/ns.gif" />
+  </noscript>
+  <!-- End TrustedForm -->
+`);
